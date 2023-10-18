1a3a442a5da8ee45614a2efef4c9bdb0501885ad
FabioPaul <94059916+FabioPaul@users.noreply.github.com>
2023-10-18 20:57:38 -0300

    limpieza

--- footer.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="footer">
-        <div class="footer-text">Todos los derechos reservados -
-            <img src="./images/copyright.png" alt="copyright-img" class="images" height="14px">Arte_polar_py
-        </div>
-    </div>
-</div>
